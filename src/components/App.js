7099891f17fc5e15d54c96a2ba9fc884aaf24a79
fatcatdog <duchenjacob@gmail.com>
2018-04-10 17:38:49 -0400

    auth finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,34 @@
-import React,  { Component }  from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
-Route
- } from 'react-router-dom';
+  Route,
+} from 'react-router-dom';
 
- import Navigation from './Navigation';
- import LandingPage from './Landing';
- import SignUpPage from './SignUp';
- import SignInPage from './SignIn';
- import PasswordForgetPage from './PasswordForget';
- import HomePage from './Home';
- import AccountPage from './Account';
+import Navigation from './Navigation';
+import LandingPage from './Landing';
+import SignUpPage from './SignUp';
+import SignInPage from './SignIn';
+import PasswordForgetPage from './PasswordForget';
+import HomePage from './Home';
+import AccountPage from './Account';
 
- import { firebase } from '../firebase';
- import * as routes from '../constants/routes';
+import * as routes from '../constants/routes';
+import withAuthentication from './withAuthentication';
 
- class App extends Component {
-   constructor(props) {
-     super(props);
-
-     this.state = {
-       authUser: null,
-     };
-   }
-
-   componentDidMount() {
-   firebase.auth.onAuthStateChanged(authUser => {
-     authUser
-       ? this.setState(() => ({ authUser }))
-       : this.setState(() => ({ authUser: null }));
-     });
-   }
-
-   render() {
-    return (
-    <Router>
+const App = () =>
+  <Router>
     <div>
-      <Navigation authUser={this.state.authUser} />
+      <Navigation />
 
-    <hr/>
+      <hr/>
 
-    <Route
-      exact path={routes.LANDING}
-      component={() => <LandingPage />}
-    />
-    <Route
-      exact path={routes.SIGN_UP}
-      component={() => <SignUpPage />}
-    />
-    <Route
-      exact path={routes.SIGN_IN}
-      component={() => <SignInPage />}
-    />
-    <Route
-      exact path={routes.PASSWORD_FORGET}
-      component={() => <PasswordForgetPage />}
-    />
-    <Route
-      exact path={routes.HOME}
-      component={() => <HomePage />}
-    />
-    <Route
-      exact path={routes.ACCOUNT}
-      component={() => <AccountPage />}
-    />
-  </div>
+      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+      <Route exact path={routes.HOME} component={() => <HomePage />} />
+      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+    </div>
   </Router>
-);
-}
-}
 
-export default App;
+  export default withAuthentication(App);
